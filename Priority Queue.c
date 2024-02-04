9d8dbc1b609556c597b3fd636fc6802773300f68
Makhdum412 <75497174+Makhdum412@users.noreply.github.com>
2024-02-04 21:48:55 +0530

    Priority Queue.c

--- Priority Queue.c
+++ Priority Queue.c
@@ -1,79 +1,105 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node{
+
+struct node {
     int data;
     int priority;
-    struct node *next;
+    struct node* next;
 };
-struct node *head=NULL;
-void enqueuePriority(){
-    struct node *ptr, *s, *w;
-    int d, p, f=0;
+
+struct node* head = NULL;
+
+void enqueuePriority() {
+    struct node* ptr, * s, * w;
+    int d, p, f = 0;
     printf("Enter the data of the queue: ");
     scanf("%d", &d);
     printf("Enter the priority of the queue: ");
     scanf("%d", &p);
-    ptr=(struct node *)malloc(sizeof(struct node));
-    ptr->data=d;
-    ptr->priority=p;
-    ptr->next=NULL;
-    if(head==NULL)
-    head=ptr;
-    else if (ptr->priority>head->priority){
-        ptr->next=head;
-        head=ptr;
-    }
-    else{
-        w=head;
-        s=head->next;
-        while(s->priority>ptr->priority){
-            if(s->next==NULL){
-                s->next=ptr;
-                f=1;
-                break;
-            }
-            else{
-                w=w->next;
-                s=s->next;
-            }
+    ptr = (struct node*)malloc(sizeof(struct node));
+    ptr->data = d;
+    ptr->priority = p;
+    ptr->next = NULL;
+
+    if (head == NULL || p > head->priority) {
+        ptr->next = head;
+        head = ptr;
+    } else {
+        w = head;
+        s = head->next;
+        while (s != NULL && s->priority > p) {
+            w = w->next;
+            s = s->next;
         }
-        if(f==0){
-            w->next=ptr;
-            ptr->next=s;
-        }
+        w->next = ptr;
+        ptr->next = s;
     }
 }
-int deletePriority(){
-    struct node *s; int t;
-    s=head;
-    t=s->data;
-    head=head->next;
+
+int deletePriority() {
+    if (head == NULL) {
+        printf("Queue is empty. Deletion not possible.\n");
+        return -1; // Return a special value to indicate an empty queue
+    }
+
+    struct node* s;
+    int t;
+    s = head;
+    t = s->data;
+    head = head->next;
     free(s);
     return t;
 }
-void display(){
-    struct node *s;
-    s=head;
-    while(s!=NULL){
+
+void display() {
+    if (head == NULL) {
+        printf("Queue is empty.\n");
+        return;
+    }
+
+    struct node* s;
+    s = head;
+    while (s != NULL) {
         printf("\t%d", s->data);
-        s=s->next;
+        s = s->next;
     }
     printf("\n");
 }
-int main(){
+
+int main() {
     int ch;
-    while(1){
-        printf("!. Insert in a priority queue\n");
+    while (1) {
+        printf("0. Exit\n");
+        printf("1. Insert in a priority queue\n");
         printf("2. Delete from a priority queue\n");
         printf("3. Display\n");
         printf("Enter your choice:");
         scanf("%d", &ch);
-        switch(ch){
-            case 1: enqueuePriority();
-            break;
-            case 2: printf("The deleted element: %d\n", deletePriority());
-            break;
-            case 3: display();
-        }
-    }
+
+        switch (ch) {
+            case 0:
+                printf("Exiting the program.\n");
+                exit(0);
+
+            case 1:
+                enqueuePriority();
+                break;
+
+            case 2:
+                {
+                    int deletedElement = deletePriority();
+                    if (deletedElement != -1) {
+                        printf("The deleted element: %d\n", deletedElement);
+                    }
+                }
+                break;
+
+            case 3:
+                display();
+                break;
+
+            default:
+                printf("Invalid choice. Please enter a valid option.\n");
+ }
+}
 }
