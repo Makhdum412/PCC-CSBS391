411b4d4f4a9e9cfe0f5ca244532e13919b78fd8c
Makhdum412 <75497174+Makhdum412@users.noreply.github.com>
2024-02-07 11:44:40 +0530

    evaluatePostfix.c

--- evaluatePostfix.c
+++ evaluatePostfix.c
@@ -7,47 +7,44 @@
 int stack[MAX_SIZE];
 int top = -1;
 
-// Function to push an element onto the stack
+
 void push(int ele) {
-	//printf("\n%d",top);
     if (top >= MAX_SIZE - 1) {
         printf("Stack Overflow\n");
         exit(1);
-    }
-    else
-    stack[++top] = ele;
-    
+    } else
+        stack[++top] = ele;
 }
 
-// Function to pop an element from the stack
+
 int pop() {
-	int t;
-	//printf("\n%d u pop",top);
     if (top == -1) {
         printf("Stack Underflow\n");
         exit(1);
+    } else {
+        return stack[top--];
     }
-    else
-    {
-    	t=stack[top--];
-	}
-    
-    return t;
 }
 
-// Function to evaluate a postfix expression
+
 int evaluatePostfix(char *expr) {
-    int i, op1, op2, res;
+    int i, op, res;
 
     for (i = 0; expr[i] != '\0'; i++) {
-    //	printf("\n%c",expr[i]);
-        if(expr[i]==' '){}
-		
-        else if (isdigit(expr[i])) {
-            push(expr[i] - '0'); // Convert character digit to integer
+        if (expr[i] == ' ') {
+            continue;
+        } else if (isdigit(expr[i])) {
+           
+            int num = 0;
+            while (isdigit(expr[i])) {
+                num = num * 10 + (expr[i] - '0');
+                i++;
+            }
+            i--; 
+            push(num);
         } else {
-            op2 = pop();
-            op1 = pop();
+            int op2 = pop();
+            int op1 = pop();
             switch (expr[i]) {
                 case '+':
                     res = op1 + op2;
@@ -69,24 +66,19 @@ int evaluatePostfix(char *expr) {
                     printf("Invalid operator: %c\n", expr[i]);
                     exit(1);
             }
-          //  printf("\n%d",res);
             push(res);
         }
     }
-   // printf("\nafter function %d",res);
-   // res=pop();
-   return pop();
+    return pop();
 }
 
 int main() {
     char expr[MAX_SIZE];
     printf("Enter a postfix expression: ");
-    //fgets(expr, MAX_SIZE, stdin);
     gets(expr);
-     
-    // printf("\nFinal top=%d",top);
-    int result =evaluatePostfix(expr); 
+
+    int result = evaluatePostfix(expr);
     printf("Result: %d\n", result);
 
-
+    return 0;
 }
